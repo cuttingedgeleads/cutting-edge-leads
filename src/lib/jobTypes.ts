b639df888f6682a7599301c625a9f9848e20e88c
Klaus <cuttingedgechatbot@gmail.com>
2026-03-02 11:26:32 -0600

    Replace 'and' with '&' in job types

--- src/lib/jobTypes.ts
+++ src/lib/jobTypes.ts
@@ -1,10 +1,10 @@
 export const JOB_TYPES = [
   "Residential Grass Maintenance",
   "Residential Landscaping",
-  "Residential Grass and Landscape Maintenance",
+  "Residential Grass & Landscape Maintenance",
   "Commercial Grass Maintenance",
   "Commercial Landscaping",
-  "Commercial Grass and Landscape Maintenance",
+  "Commercial Grass & Landscape Maintenance",
 ] as const;
 
 export type JobType = (typeof JOB_TYPES)[number];
